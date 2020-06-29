7adc26e3c5287aa52de0d98dc59e360680b38fa1
Lorne Ashley <lorne.b.b@gmail.com>
2020-06-29 17:26:43 +0000

    added js to send mail - tests prove it is NOT working yet. to be fixed.

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,1 +1,16 @@
-console.log("hello");
+function sendMail(contactForm) {
+    emailjs.send("gmail", "wub", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.feature_request.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
