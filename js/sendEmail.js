b46cb63efcac4a44bef9393c34ca15e8b14362ba
Lorne Ashley <lorne.b.b@gmail.com>
2020-06-30 12:54:33 +0000

    bug fixing js for emailjs - not working yet

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,26 +1,8 @@
-var template_params = {
+const template_params = {
    "feature_request": "feature_request_value"
 };
-
-var service_id = "default_service";
-var template_id = "wub";
-
+let service_id = "default_service";
+let template_id = "wub";
 emailjs.send(service_id, template_id, template_params)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    return false;});
-
-/* function sendMail(contactForm) {
-    emailjs.send("gmail", "wub", {
-        "project_request": contactForm.feature_request.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response.status, response.text);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
-*/
+    .then(res => {console.log('SUCCESS!', response.status, response.text);
+    }).catch(error => {console.log('NOPE, try again')});
