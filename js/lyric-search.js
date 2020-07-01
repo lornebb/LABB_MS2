1d16eccf0d7c531bb6c6cfa5a27869343c2c0bef
Lorne Ashley <lorne.b.b@gmail.com>
2020-07-01 13:44:26 +0000

    building js lyric search api

--- js/lyric-search.js
+++ js/lyric-search.js
@@ -1,15 +1,17 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://shazam.p.rapidapi.com/search?locale=en-US&offset=0&limit=5&term=endicott",
+function searchLyrics(lyricSearch) {
+fetch("https://shazam.p.rapidapi.com/search?locale=en-US&offset=0&limit=5&term=kiss%20the%20rain", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "shazam.p.rapidapi.com",
 		"x-rapidapi-key": "15dac825ebmsh76a9e21a4637745p1a6c3bjsnb08d198023c0"
 	}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+})
+.then(response => {
+    console.log(response)
+    console.log("it worked");
+})
+.catch(err => {
+    console.log(err)
+    console.log("try again stupid");
+})};
 
