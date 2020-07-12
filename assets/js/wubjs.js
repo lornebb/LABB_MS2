6ba903def76e3125e7593031bba86a02612f803d
lornebb <51881266+lornebb@users.noreply.github.com>
2020-07-12 12:37:37 +0100

    first iteration of working chord chart display

--- assets/js/wubjs.js
+++ assets/js/wubjs.js
@@ -1,5 +1,4 @@
 // send mail js
-// 
 
 (function () {
     emailjs.init("user_NLgvc4Mu5hpwy6V4uUBBn");
@@ -104,6 +103,143 @@ function randomChords() {
 
 generateButtonRandomCP.addEventListener("click", randomChords);
 
+// Chord Chart generator
+
+$(document).ready(() => {
+
+    const chordSelectGtr = (chord) => {
+        return `<ins class="scales_chords_api" chord="${chord} instrument="guitar"></ins>`
+    }
+
+    $("#chord-dropdown").on("change", function (e) {
+        const newChord = e.target.value;
+        const location = $(".gtr-hide");
+        location.html(chordSelect(newChord));
+        scales_chords_api_onload();
+    });
+
+    function playAudio3(id_number) {
+        var button = document.getElementById('playbut_' + id_number);
+        button.style.background = "url('https://www.scales-chords.com/images/snd-mute-green-2.png') no-repeat";
+        button.style.backgroundSize = "100%";
+        var music = document.getElementById('music' + id_number);
+        music.play();
+    }
+    
+    function audioStop(id_number) {
+        var button = document.getElementById('playbut_' + id_number);
+        button.style.background = "url('https://www.scales-chords.com/images/snd-play-green-2.png') no-repeat";
+        button.style.backgroundSize = "100%";
+    }
+    
+    function scales_chords_api_onload() {
+        var api_url = "https://www.scales-chords.com/api/scapi.1.3.php";
+        if (typeof api_override_url !== 'undefined') {
+            var api_url = api_override_url;
+        }
+        if (typeof scales_chords_api_debug === 'undefined') {
+            var scales_chords_api_debug = false;
+        }
+        var x = document.getElementsByClassName("scales_chords_api");
+        var obj_id_count = 1;
+        if (scales_chords_api_debug) console.log(x);
+        for (var i = 0; i < x.length; i++) {
+            var params = "";
+            var first = true;
+            var obj = x[i];
+            obj.id = "scapiobjid" + obj_id_count;
+            obj_id_count += 1;
+            var att = x[i].attributes;
+            if (scales_chords_api_debug) console.log(x[i]);
+            if (scales_chords_api_debug) console.log(att);
+            if (scales_chords_api_debug) console.log(att.length);
+            for (j = 0; j < att.length; j++) {
+                if (scales_chords_api_debug) console.log(att[j]);
+                if (scales_chords_api_debug) console.log("name: " + att[j].nodeName + " value: " + att[j].nodeValue);
+                if (!first) params += "&";
+                else params = "id=" + obj.id + "&";
+                first = false;
+                params += encodeURI(att[j].nodeName) + "=" + encodeURI(att[j].nodeValue);
+            }
+            if (scales_chords_api_debug) alert(params);
+            ajaxCall(api_url, params, function (xmlhttp) {
+                var myString = xmlhttp.responseText;
+                var myStringArray = myString.split("###RAWR###");
+                var objid = myStringArray[0];
+                if (scales_chords_api_debug) {
+                    document.getElementById(objid).innerHTML = "<!--" + mystring + "-->";
+                    if (myStringArray[2].length > 0) document.getElementById(objid).innerHTML += myStringArray[2];
+                } else {
+                    if (myStringArray[2].length > 0)
+                        document.getElementById(objid).innerHTML = myStringArray[2];
+                }
+            });
+        }
+    }
+    
+    if (window.addEventListener) {
+        window.addEventListener('load', scales_chords_api_onload, false);
+    } else {
+        window.attachEvent('onload', scales_chords_api_onload);
+    }
+    
+    function ajaxCall(url, params, successCallback, failCallback, ongoingCallback, cfg) {
+        var xhr;
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    if (successCallback) {
+                        successCallback(xhr);
+                    }
+                } else {
+                    if (failCallback) {
+                        failCallback(xhr);
+                    }
+                }
+            } else {
+                if (ongoingCallback) {
+                    ongoingCallback(xhr);
+                }
+            }
+        };
+        var paramString = "";
+        if (typeof params === "string")
+            paramString = params;
+        else
+        if (params && typeof params === "object") {
+            for (var p in params) {
+                var pValue = params[p];
+                if (typeof pValue === "string")
+                    pValue = encodeURIComponent(pValue);
+                paramString += "&" + p + "=" + pValue;
+            }
+            if (paramString.length > 0)
+                paramString = paramString.substring(1);
+        }
+        xhr.open("POST", url, true);
+        if (cfg) {
+            if (cfg.options) {
+                for (var o in cfg.options) {
+                    if (o in xhr)
+                        xhr[o] = cfg.options[o];
+                }
+            }
+            if (cfg.headers) {
+                for (var h in cfg.headers) {
+                    xhr.setRequestHeader(h, cfg.headers[h]);
+                }
+            }
+        }
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(paramString);
+    }
+});
+
 // search lyrics and show results js 
 
 function searchLyrics(searchValue) {
@@ -128,14 +264,9 @@ function searchLyrics(searchValue) {
             $('#results-below-box-show').css("display", "block")
             console.log(data)
             var artistResult = data.tracks.hits[0].track
-            // console.log("artist:", data.tracks.hits[0].track.subtitle)
-            // console.log("track:", data.tracks.hits[0].track.title)
-            // console.log("link", data.tracks.hits[0].track.share.html)
             var lyricResultLink = data.tracks.hits[0].track.share.html
-            // console.log("did it work?", lyricResultLink)
             $('#artist-result').val(`${artistResult.subtitle}`)
             $('#track-result').val(`${artistResult.title}`)
-            //  $('#lyric-link').attr("href", `${artistResult.track.share.html}`)
             $("#lyric-link").find("href").attr("href", `${lyricResultLink}`);
         })
         .catch(err => {
@@ -150,33 +281,6 @@ $('#lyric-search-form').submit(function (e) {
     let searchValue = $('#lyric-search-box').val()
     searchLyrics(searchValue);
 });
-
-// Chord chart 
-
-function returnChordsToHtml() {
-    let chordName = document.getElementById("chordName")
-    return chordName;
-}
-
-function showGtrChords() {
-    let chordName = document.getElementById("chordName")
-    console.log(chordName.value)
-    let chRoot = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'];
-    let chQuality = ['', 'm', 'dim', '+'];
-    //$("#scales_chords_api").find("chord").attr("chord", `${chordName}`)
-    // var test = $(chordName).val();
-    // console.log(test)
-    if (chordName !== chRoot || chordName !== chQuality) {
-        fetch(``)
-
-
-        // <ins class="scales_chords_api" chord="${chordName}" instrument="guitar" width="390px;" height="200px;"></ins>
-        document.getElementById('gtr-insert').insertAdjacentHTML('beforeend', ``);
-    } else {
-        console.log("gtr choord search did not work")
-        document.getElementById('gtr-insert').insertAdjacentHTML('beforeend', `<h6 class="small-text"> Your search for ${chordName} did not return anything, try a less specific syntax." </h6>`);
-    };
-};
 
 function showPianoChords() {
     let chordName = document.getElementById("chordName")
