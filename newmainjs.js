9db7816c9c1a1149ee1704d9da7746a6ce0a4b1e
lornebb <51881266+lornebb@users.noreply.github.com>
2020-07-15 17:16:29 +0100

    impimented Lyric Search function - Shazam Api with correct headers - changed html to include ids and button links for results filling. - added loader icon on send for search results

--- newmainjs.js
+++ newmainjs.js
@@ -16,15 +16,19 @@ $(document).ready(function () {
         $('#chord-section').hide();
         $('#lyric-section').hide();
     });
+    $("#lyric-search-results").hide();
+    $("#lyric-search-loading").hide();
 });
 
-// emailJS
+// emailJS API
 $('#contact-form-submit').on('click', sendMail)
 
 function sendMail(contactForm) {
     console.log("EmailJS - calling");
     emailjs.init("user_NLgvc4Mu5hpwy6V4uUBBn");
-    let templateParams = {"feature_request": $('#feature-request').val()}
+    let templateParams = {
+        "feature_request": $('#feature-request').val()
+    }
     emailjs.send("lorneashley_gmail_com", "wub", templateParams)
         .then(
             function (response) {
@@ -37,3 +41,44 @@ function sendMail(contactForm) {
     return false;
 }
 
+// Lyric Search - Shazam API
+
+$("#lyric-form-submit").on('click', searchLyrics)
+
+function searchLyrics(searchValue) {
+    $("#lyric-search-loading").show()
+    fetch(`https://shazam.p.rapidapi.com/search?locale=en-US&offset=0&limit=5&term=${searchValue}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "shazam.p.rapidapi.com",
+                "x-rapidapi-key": "15dac825ebmsh76a9e21a4637745p1a6c3bjsnb08d198023c0",
+            }
+        })
+        .then(response => response.json()
+            .then(data => {
+                $('#lyric-search-loading').hide()
+                $('#lyric-search-results').show()
+                console.log(data)
+                let artistResult = data.tracks.hits[0].track
+                let lyricResultLink = data.tracks.hits[0].track.share.html
+                $('#artist-fill').html(`${artistResult.subtitle}`)
+                $('#song-fill').html(`${artistResult.title}`)
+                console.log(lyricResultLink)
+                $("#lyric-search-link").attr("href", `${lyricResultLink}`)
+            })
+            .catch(err => {
+                $('#something-went-wrong-box-hide').css("display", "block")
+                $("#search-loading").css("display", "none")
+                console.log(err)
+            }))
+};
+
+// $(selector).attr(attributeName, value);
+
+
+// $('#lyric-search-form').submit(function (e) {
+//     e.preventDefault();
+//     $("#search-loading").toggle();
+//     let searchValue = $('#lyric-search-box').val()
+//     searchLyrics(searchValue);
+// });
