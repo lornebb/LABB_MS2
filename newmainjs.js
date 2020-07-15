5fe7c91f0316d816140c70746bc4a2c7e8bfa10f
lornebb <51881266+lornebb@users.noreply.github.com>
2020-07-15 14:53:18 +0100

    - removed selectall js, - applied jquery show and hide JS

--- newmainjs.js
+++ newmainjs.js
@@ -1,2 +1,19 @@
-const sections = document.querySelectorAll('.section-selector');
-console.log(sections);
+// section toggles
+$(document).ready(function () {
+    $('#chord-section').hide();
+    $('#lyric-section').hide();
+    $('#chords-section-button').on('click', function () {
+        $('#home-section').hide();
+        $('#chord-section').show();
+    });
+    $('#lyric-section-button').on('click', function () {
+        $('#lyric-section').show();
+        $('#home-section').hide();
+        $('#chord-section').hide();
+    });
+    $('.back').on('click', function () {
+        $('#home-section').show();
+        $('#chord-section').hide();
+        $('#lyric-section').hide();
+    });
+});
