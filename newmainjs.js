3b388655407b1dd6816679c9f3bea1d7a651ce74
lornebb <51881266+lornebb@users.noreply.github.com>
2020-07-19 16:00:16 +0100

    move to dropdown option box for chord selector - try out. added html pen for tidy code store

--- newmainjs.js
+++ newmainjs.js
@@ -44,59 +44,62 @@ function sendMail() {
 }
 
 // Chord Generator
-$(document).ready(() => {
-    let noChords = $(".no-of-chords")
-    let noProgressions = $(".no-of-progressions")
-    let generateButtonRandomChP = $("#generate-random-ChP");
+// only one doc.ready script in the whole document? - ask simen.
 
-    function chordGenerator(chNum) {
-        let replaceObj = {
-            'Ebdim': 'D#dim',
-            'Abdim': 'G#dim',
-            'Bbdim': 'A#dim'
-        };
-        let chRoot = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'];
-        let chQuality = ['', 'm', 'dim', '+'];
-        let chProg = '';
-        for (let i = 0; i < chNum; i++) {
-            let randRoot = randomChoice(chRoot);
-            let randQuality = randomChoice(chQuality);
-            let chord = randRoot + randQuality;
-            if (chord in replaceObj) {
-                chord = replaceObj[chord];
-            }
-            chProg += chord + ((i < chNum - 1) ? " | " : "");
+let noChords = $("#chord-amount-selector")
+let noProgressions = $("#progression-amount-selector")
+let generateButtonRandomChP = $("#generate-random-ChP");
+
+function chordGenerator(chNum) {
+    let replaceObj = {
+        'Ebdim': 'D#dim',
+        'Abdim': 'G#dim',
+        'Bbdim': 'A#dim'
+    };
+    let chRoot = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'];
+    let chQuality = ['', 'm', 'dim', '+'];
+    let chProg = '';
+    for (let i = 0; i < chNum; i++) {
+        let randRoot = randomChoice(chRoot);
+        let randQuality = randomChoice(chQuality);
+        let chord = randRoot + randQuality;
+        if (chord in replaceObj) {
+            chord = replaceObj[chord];
         }
-        return chProg;
+        chProg += chord + ((i < chNum - 1) ? " | " : "");
     }
+    return chProg;
+}
 
-    function randomChords() {
-        let chNum = Number(noChords.value);
-        let progNum = Number(noProgressions.value);
-        if (chNum < 1) {
-            chNum = 1;
-        }
-        if (chNum > 16) {
-            chNum = 16;
-        }
-        if (progNum < 1) {
-            progNum = 5;
-        }
-        if (progNum > 50) {
-            progNum = 50;
-        }
-        let titleOutput = `${progNum} Random Progressions of ${chNum} Chords`;
-        let output = "";
-        for (let i = 1; i <= progNum; i++) {
-            output += `${i}.` + ((i < 10) ? "\xa0\xa0\xa0" : "\xa0\xa0");
-            output += `${chordGenerator(chNum)}<br>`;
-        }
-        document.getElementById("titleRandomCP").innerHTML = titleOutput;
-        document.getElementById("functionRandomCP").innerHTML = output;
+function randomChords() {
+    console.log(noChords.value)
+    let chNum = Number(noChords.value);
+    console.log("2nd call" + noChords.value)
+    let progNum = Number(noProgressions.value);
+    if (chNum < 1) {
+        chNum = 1;
     }
+    if (chNum > 16) {
+        chNum = 16;
+    }
+    if (progNum < 1) {
+        progNum = 5;
+    }
+    if (progNum > 50) {
+        progNum = 50;
+    }
+    let titleOutput = `${progNum} Random Progressions of ${chNum} Chords`;
+    let output = "";
+    for (let i = 1; i <= progNum; i++) {
+        output += `${i}.` + ((i < 10) ? "\xa0\xa0\xa0" : "\xa0\xa0");
+        output += `${chordGenerator(chNum)}<br>`;
+    }
+    $("#titleRandomCP").innerHTML = titleOutput;
+    $("#functionRandomCP").innerHTML = output;
+}
 
-   generateButtonRandomChP.addEventListener("click", randomChords);
-});
+generateButtonRandomChP.on("click", randomChords);
+
 
 
 // Lyric Search - Shazam API
